449e5fcb8385212d7aab39c75b9fd83230187335
Eliam Pimentel <eliampimentel@prodominicana.gob.do>
2023-07-19 13:22:04 -0400

    Tarjetas como componente

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import Card from "@/components/home/card";
+import TypeWriterEffect from "react-typewriter-effect";
 
 export default function Home() {
+  const desc = `El Sistema Nacional de Inteligencia de Mercados de la Dirección de
+            Inteligencia de Mercados de ProDominicana, es un conjunto de
+            subsistemas de alertas, oportunidades, amenazas y obstaculos que
+            pueda enfrentar el comercio exterior y la atraccién de inversion
+            extranjera directa (IED) de la Republica Dominicana.`;
+
   return (
     <main className="min-h-screen h-screen relative">
       <div className="absolute inset-0 z-[-1]">
@@ -9,7 +19,7 @@ export default function Home() {
           autoPlay
           loop
           muted
-          className="w-full h-screen fixed object-cover"
+          className="w-full h-screen fixed object-cover transition-opacity duration-500"
         >
           <source src="/videos/world.mp4" type="video/mp4"></source>
         </video>
@@ -43,19 +53,21 @@ export default function Home() {
       </nav>
 
       <div className="flex h-full md:h-5/6 md:p-5 ml-0 md:justify-start justify-center items-center md:absolute">
-        <div className="flex flex-col space-y-4 mx-10">
+        <div className="flex flex-col space-y-4 mx-10 ">
           <Image
             src={"/images/logo/sinim.png"}
             width={300}
             height={300}
             alt=""
           ></Image>
-          <p className="md:w-2/6 md:text-base text-xs md:block hidden">
-            El Sistema Nacional de Inteligencia de Mercados de la Direccién de
-            Inteligencia de Mercados de ProDominicana, es un conjunto de
-            subsistemas de alertas, oportunidades, amenazas y obstaculos que
-            pueda enfrentar el comercio exterior y la atraccién de inversion
-            extranjera directa (IED) de la Republica Dominicana.
+          <p className="md:w-[32rem] md:text-base text-xs md:block hidden">
+            <TypeWriterEffect
+              startDelay={100}
+              cursorColor="white"
+              text={desc}
+              typeSpeed={20}
+              className="w-full"
+            />
           </p>
           <Link href="/" className="md:block hidden">
             <div className="w-32 text-center text-xl bg-white text-black rounded-full px-5 py-1 duration-500">
@@ -66,42 +78,26 @@ export default function Home() {
       </div>
       <div className="h-screen flex justify-center items-end">
         <div className="flex md:flex-row flex-col w-full md:h-44 h-full md:space-x-5 m-10 md:space-y-0 space-y-10 overflow-y-auto">
-          <button className="flex justify-center items-center md:h-full h-40 md:w-full bg-gradient-to-b from-green-500 to-sky-600 rounded-lg text-2xl hover:text-3xl duration-300">
-            <Image
-              src={"/svg/datamarketicon.svg"}
-              width={100}
-              height={100}
-              alt=""
-            ></Image>
-            <div className="absolute font-custom">DATA MARKET</div>
-          </button>
-          <button className="flex justify-center items-center md:h-full h-40 md:w-full bg-gradient-to-b from-pink-600 to-violet-800 rounded-lg text-2xl hover:text-3xl duration-300">
-            <Image
-              src={"/svg/siedicon.svg"}
-              width={100}
-              height={100}
-              alt=""
-            ></Image>
-            <div className="absolute font-custom">SIED</div>
-          </button>
-          <button className="flex justify-center items-center md:h-full h-40 md:w-full bg-gradient-to-b from-sky-500 to-purple-700 rounded-lg text-2xl hover:text-3xl duration-300">
-            <Image
-              src={"/svg/saimicon.svg"}
-              width={100}
-              height={100}
-              alt=""
-            ></Image>
-            <div className="absolute font-custom ">SAIM</div>
-          </button>
-          <button className="flex justify-center items-center md:h-full h-40 md:w-full bg-gradient-to-b from-yellow-400 to-red-500 rounded-lg text-2xl hover:text-3xl duration-300">
-            <Image
-              src={"/svg/ramiicon.svg"}
-              width={100}
-              height={100}
-              alt=""
-            ></Image>
-            <div className="absolute font-custom ">RAMI</div>
-          </button>
+          <Card
+            title={"DATA MARKET"}
+            image={`/svg/datamarketicon.svg`}
+            color={`bg-gradient-to-b from-green-500 to-sky-600`}
+          />
+          <Card
+            title={"SIED"}
+            image={`/svg/siedicon.svg`}
+            color={`bg-gradient-to-b from-pink-600 to-violet-800`}
+          />
+          <Card
+            title={"SAIM"}
+            image={`/svg/saimicon.svg`}
+            color={`bg-gradient-to-b from-sky-500 to-purple-700`}
+          />
+          <Card
+            title={"RAMI"}
+            image={`/svg/ramiicon.svg`}
+            color={`bg-gradient-to-b from-yellow-400 to-red-500`}
+          />
         </div>
       </div>
     </main>
---
+++ components/home/card.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Card({
+  image,
+  title,
+  color,
+  icon,
+  initialY,
+  time,
+}: any) {
+  return (
+    <button
+      className={`flex justify-center items-center md:h-full h-40 md:w-full ${color} rounded-lg text-2xl hover:text-3xl duration-300`}
+    >
+      <Image src={`${image}`} width={100} height={100} alt=""></Image>
+      <div className="absolute font-custom">{title}</div>
+    </button>
+  );
+}
