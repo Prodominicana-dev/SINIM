33195690261afb39b3743159e6f8997a5a3efa6e
Randy Frias <randyfrias@prodominicana.gob.do>
2023-08-07 16:05:22 -0400

    .

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       {status === "loading" ? (
         <p>Cargando...</p>
       ) : session ? (
-        <div>{session.user?.name}</div>
+        <div>{session.user?.email}</div>
       ) : (
         <h1 className="text-5xl">NO SE PUEDE MIO</h1>
       )}
