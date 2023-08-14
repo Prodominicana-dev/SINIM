33ed683c7b3d40a23536a7cd29e9d1798a563184
eliampimentel <139479508+eliampimentel@users.noreply.github.com>
2023-08-14 15:08:53 -0400

    last v
    Estable

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,25 +1,29 @@
 import Background from '@/src/components/home/background'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
     <>
       <Background video={'/videos/rami.mp4'} color={'bg-gradient-to-l from-blue-700 from-[40%] to-sky-500/70'} />
-      <div className='w-full h-screen'>
+      <div className='w-full h-screen '>
         <div className='w-6/12 h-full ml-auto flex flex-col justify-center items-center'>
-          <div>
-
+          <div className='w-full flex justify-center '>
           <Image
           src={'/images/logo/sinim.png'}
           width={500}
           height={500}
           draggable={false}
           alt=""
-          className="w-full "
+          className="w-96"
         />
           </div>
-        
+          <div className='w-10/12 h-20 flex flex-row justify-center mt-8 space-x-8'>
+            <Link href={"/api/auth/login"} className='flex justify-center items-center text-lg text-blue-700 w-64 h-16 bg-white hover:bg-gray-100 duration-200 shadow-md rounded-[30px]
+            '>Iniciar sesión</Link>
+            <Link href={"/api/auth/signup"} className='flex justify-center items-center text-lg text-blue-700 w-64 h-16 bg-white hover:bg-gray-100 duration-200 shadow-md rounded-[30px]'>Crear cuenta</Link>
+          </div>
         </div>
       </div>
     </>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -113,14 +113,10 @@ export default function Home() {
             </button>
           </div>
           <div className="hidden sm:flex items-center space-x-5">
-            <Link href="/auth/login" className="md:block hidden">
+        
+            <Link href="/dashboard" className="md:block hidden">
               <div className="w-28 text-center text-lg rounded-full border-2 border-white px-5 py-2 hover:shadow-button duration-500">
-                Log in
-              </div>
-            </Link>
-            <Link href="/auth/register" className="md:block hidden">
-              <div className="w-28 text-center text-lg rounded-full border-2 border-white px-5 py-2 hover:shadow-button duration-500">
-                Sign in
+                Acceder
               </div>
             </Link>
           </div>
