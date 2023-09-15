9461961776c3aa6c524e8b2b2b444f23f79f7f12
Eliam Pimentel <eliampimentel@prodominicana.gob.do>
2023-09-15 15:47:01 -0400

    countries service

--- src/services/countries/getCountries.tsx
+++ src/services/countries/getCountries.tsx
@@ -2,9 +2,9 @@ import country from '@/src/models/country';
 import axios from 'axios'
 
 export default async function getCountries() : Promise<country> { 
-    const saimEndpoint = `http://127.0.0.1:3001/countries`;
-    const response = await axios.get(saimEndpoint);
-    const products = response.data;
-    const data = products.map((item: country) => item);
+    const url = `http://127.0.0.1:3001/countries`;
+    const response = await axios.get(url);
+    const countries = response.data;
+    const data = countries.map((item: country) => item);
     return data;
 }
