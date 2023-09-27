89662edad4ebba9d8b9c0234747d6fe5578a3c63
Randy Frias <randyfrias@prodominicana.gob.do>
2023-09-27 12:59:40 -0400

    rami dialog responsive updated

--- src/components/rami/modal/Modal.tsx
+++ src/components/rami/modal/Modal.tsx
@@ -13,7 +13,7 @@ import {
   TabsBody,
   TabsHeader,
 } from "@material-tailwind/react";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -22,6 +22,55 @@ import { data } from "autoprefixer";
 
 export default function Modal({ id }: any) {
   const { data, isLoading, isError } = useRami(id);
+  const [ramiData, setRamiData] = useState<any>([]);
+  useEffect(() => {
+    if (!isLoading) {
+      const ramidata = [
+        {
+          label: "Requerimientos salida",
+          value: "salida",
+          desc: data.outputRequirement,
+        },
+        {
+          label: "Requisitos importacion",
+          value: "importacion",
+          desc: data.importRequirement,
+        },
+        {
+          label: "Regulaciones tecnicas",
+          value: "regulaciones",
+          desc: data.technicalRequirements,
+        },
+        {
+          label: "Permisos y certificaciones",
+          value: "certificaciones",
+          desc: data.permitsCertifications,
+        },
+        {
+          label: "Etiquetado",
+          value: "etiquetado",
+          desc: data.labelingCertifications,
+        },
+        {
+          label: "Acuerdos comerciales",
+          value: "acuerdos",
+          desc: data.tradeAgreement,
+        },
+        {
+          label: "Impuestos y aranceles",
+          value: "aranceles",
+          desc: data.tariffsImposed,
+        },
+        {
+          label: "Recursos web",
+          value: "recursosweb",
+          desc: data.webResource,
+        },
+      ];
+      setRamiData(ramidata);
+    }
+  }, [data]);
+
   const [open, setOpen] = useState(true);
   const router = useRouter();
   const handleOpen = useCallback(() => {
@@ -57,14 +106,14 @@ export default function Modal({ id }: any) {
       <DialogBody className="flex justify-center h-[100vm] overflow-y-auto">
         <div className="w-11/12">
           <div className="flex flex-col sm:flex-row justify-between items-center w-full  sm:h-64 lg:h-96 bg-gradient-to-tr from-purple-700 to-sky-500 rounded-lg sm:rounded-xl lg:rounded-3xl p-5 sm:p-8 text-white">
-            <div className="flex flex-col leading-normal w-10/12 sm:w-6/12">
+            <div className="flex flex-col leading-normal w-full sm:w-6/12">
               <div className="text-xs sm:text-sm lg:text-lg">Exporta</div>
               <div className="text-2xl lg:text-6xl">{data.product.name}</div>
               <div className="text-xs lg:text-sm mb-2 sm:my-0 ">
                 {data.product.code}
               </div>
             </div>
-            <div className="flex flex-col justify-between bg-white/25 w-10/12 sm:w-5/12 h-48 sm:h-full rounded-lg p-3 sm:p-5">
+            <div className="flex flex-col justify-between bg-white/25 w-full sm:w-5/12 h-52 sm:h-full rounded-lg p-3 sm:p-5">
               <div>
                 <div className="text-xs lg:text-sm">Destino</div>
                 <div className="text-lg lg:text-4xl">{data.country.name}</div>
@@ -75,12 +124,21 @@ export default function Modal({ id }: any) {
                 alt=""
                 priority
                 src={`https://flagcdn.com/${data.country.abbreviation}.svg`}
-                className="rounded-md lg:rounded-lg w-full h-28 sm:h-24 lg:h-52 object-cover"
+                className="rounded-md lg:rounded-lg w-full h-32 sm:h-24 lg:h-52 object-cover"
               />
             </div>
           </div>
           <div className="pt-3">
-            <UnderlineTabs {...data} />
+            <div className="hidden sm:block">
+              <UnderlineTabs data={ramiData} />
+            </div>
+            <div className="block sm:hidden">
+              {ramiData.map(({ label, value, desc }: any) => (
+                <div className="p-2">
+                  <SectionRami title={label} desc={desc} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </DialogBody>
@@ -88,50 +146,9 @@ export default function Modal({ id }: any) {
   );
 }
 
-function UnderlineTabs(rami: any) {
+function UnderlineTabs({ data }: any) {
   const [activeTab, setActiveTab] = useState("salida");
-  const data = [
-    {
-      label: "Requerimientos salida",
-      value: "salida",
-      desc: rami.outputRequirement,
-    },
-    {
-      label: "Requisitos importacion",
-      value: "importacion",
-      desc: rami.importRequirement,
-    },
-    {
-      label: "Regulaciones tecnicas",
-      value: "regulaciones",
-      desc: rami.technicalRequirements,
-    },
-    {
-      label: "Permisos y certificaciones",
-      value: "certificaciones",
-      desc: rami.permitsCertifications,
-    },
-    {
-      label: "Etiquetado",
-      value: "etiquetado",
-      desc: rami.labelingCertifications,
-    },
-    {
-      label: "Acuerdos comerciales",
-      value: "acuerdos",
-      desc: rami.tradeAgreement,
-    },
-    {
-      label: "Impuestos y aranceles",
-      value: "aranceles",
-      desc: rami.tariffsImposed,
-    },
-    {
-      label: "Recursos web",
-      value: "recursosweb",
-      desc: rami.webResource,
-    },
-  ];
+
   return (
     <Tabs value={activeTab}>
       <TabsHeader
@@ -141,7 +158,7 @@ function UnderlineTabs(rami: any) {
             "bg-gradient-to-r from-purple-700 to-sky-500 shadow-none rounded-none w-[80vw] sm:w-[9vw] lg:w-[8vw]",
         }}
       >
-        {data.map(({ label, value }) => (
+        {data.map(({ label, value }: any) => (
           <Tab
             key={value}
             value={value}
@@ -157,12 +174,23 @@ function UnderlineTabs(rami: any) {
         ))}
       </TabsHeader>
       <TabsBody>
-        {data.map(({ value, desc }) => (
+        {data.map(({ label, value, desc }: any) => (
           <TabPanel key={value} value={value}>
-            <div dangerouslySetInnerHTML={{ __html: desc }}></div>
+            <SectionRami title={label} desc={desc} />
           </TabPanel>
         ))}
       </TabsBody>
     </Tabs>
+  );
+}
+
+function SectionRami({ title, desc }: any) {
+  return (
+    <div>
+      <div className="text-2xl font-semibold bg-gradient-to-r from-purple-700 to-sky-500  inline-block text-transparent bg-clip-text mb-3">
+        {title}
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: desc }}></div>
+    </div>
   );
 }
