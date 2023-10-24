085d4cd2e4fddd8b9ad755131ce4ee7b16d8a7b1
Randy Frias <randyfrias@prodominicana.gob.do>
2023-10-24 11:14:50 -0400

    users

---
+++ src/components/settings/users/card.tsx
@@ -0,0 +1,111 @@
+import {
+  EyeIcon,
+  EyeSlashIcon,
+  MinusIcon,
+  PencilSquareIcon,
+  PlusIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
+import User from "@/src/models/user";
+import UserDialog from "./dialog";
+import HideButton from "../hide";
+import ActiveButton from "../active";
+import { Avatar } from "@material-tailwind/react";
+
+export default function Card({
+  user,
+  updateUsers,
+}: {
+  user: User;
+  updateUsers: () => void;
+}) {
+  const [open, setOpen] = useState(false);
+  const [mode, setMode] = useState(true);
+  const handleOpen = (mode: boolean) => {
+    setMode(mode);
+    setOpen(!open);
+  };
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const handleDeleteOpen = () => setDeleteOpen(!deleteOpen);
+  const deleteCreateNotification = {
+    title: "Usero ocultado",
+    message: "El Usuario ha sido ocultado exitosamente.",
+    color: "green",
+  };
+
+  const deleteErrorNotification = {
+    title: "Error eliminando el Ususuario",
+    message: "Ha ocurrido un error, intenta nuevamente.",
+    color: "red",
+  };
+
+  const [activeOpen, setActiveOpen] = useState(false);
+  const handleActiveOpen = () => setActiveOpen(!deleteOpen);
+  const handleActiveClose = () => setActiveOpen(false);
+  const activeCreateNotification = {
+    title: "Usero activado",
+    message: "El Usuario ha sido activado exitosamente.",
+    color: "green",
+  };
+
+  const activeErrorNotification = {
+    title: "Error activando el Usuario",
+    message: "Ha ocurrido un error, intenta nuevamente.",
+    color: "red",
+  };
+
+  return (
+    <>
+      <div className="grid items-center w-full h-24 grid-cols-4 p-5 text-center bg-white rounded-lg ring-2 ring-gray-100">
+        <div className="flex justify-center">
+          <Avatar src={`${user.picture}`}></Avatar>
+        </div>
+        <div className="line-clamp-2">{user.name}</div>
+        <div className="line-clamp-1">{user.email}</div>
+        <div className="flex justify-center space-x-5 ">
+          <button
+            onClick={() => handleOpen(true)}
+            className="flex items-center justify-center text-black bg-white rounded-lg w-14 h-14 ring-1 ring-gray-100"
+          >
+            <PlusIcon className="w-7" />
+          </button>
+          <button
+            onClick={() => handleOpen(false)}
+            className="flex items-center justify-center text-black bg-white rounded-lg w-14 h-14 ring-1 ring-gray-100"
+          >
+            <MinusIcon className="w-7" />
+          </button>
+        </div>
+      </div>
+      <HideButton
+        open={deleteOpen}
+        handleOpen={handleDeleteOpen}
+        update={updateUsers}
+        title={"¿Estás seguro de ocultar este User?"}
+        message="El User será ocultado y podrá ser activado."
+        endpoint={`/user/deactive/${user.user_id}`}
+        createNotification={deleteCreateNotification}
+        errorNotification={deleteErrorNotification}
+      />
+      <ActiveButton
+        open={activeOpen}
+        handleOpen={handleActiveClose}
+        update={updateUsers}
+        title={"¿Estás seguro de activar este User?"}
+        message="El User será activado y cualquier persona podría verlo."
+        endpoint={`/user/active/${user.user_id}`}
+        createNotification={activeCreateNotification}
+        errorNotification={activeErrorNotification}
+      />
+      {open ? (
+        <UserDialog
+          open={open}
+          mode={mode}
+          handleOpen={() => handleOpen(mode)}
+          updateUser={updateUsers}
+          user={user}
+        />
+      ) : null}
+    </>
+  );
+}
---
+++ src/components/settings/users/dialog.tsx
@@ -0,0 +1,125 @@
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  Input,
+  Avatar,
+} from "@material-tailwind/react";
+import Select from "react-select";
+import { useEffect, useState } from "react";
+import User from "@/src/models/user";
+import {
+  assignUserRoles,
+  removeUserRoles,
+  useRoles,
+  useUserRoles,
+} from "@/src/services/users/service";
+import makeAnimated from "react-select/animated";
+
+export default function UserDialog({
+  user,
+  open,
+  mode,
+  handleOpen,
+  updateUser,
+}: {
+  user: User;
+  open: boolean;
+  mode: boolean;
+  handleOpen: () => void;
+  updateUser: () => void;
+}) {
+  const [userName, setUserName] = useState<any>(user?.nickname);
+  const [userEmail, setUserEmail] = useState<any>(user?.email);
+  const [selectedRoles, setSelectedRoles] = useState<any>([]);
+  const [rolesList, setRolesList] = useState<any>([]);
+  const { data: userRoles } = useUserRoles(user?.user_id);
+  const { data: roles } = useRoles();
+  const animatedComponents = makeAnimated();
+
+  useEffect(() => {
+    if (user && userRoles && roles) {
+      setUserName(user.name);
+      setUserEmail(user.email);
+      console.log(userRoles);
+      const userRole = userRoles.map((rol: any) => {
+        return { value: rol.id, label: rol.name };
+      });
+      const allRoles = roles.map((rol: any) => {
+        return { value: rol.id, label: rol.name };
+      });
+      setSelectedRoles(userRole);
+      setRolesList(allRoles);
+    }
+  }, [userRoles, roles, user]);
+
+  const handleUserSubmit = async () => {
+    const roleIds = selectedRoles.map((role: any) => role.value);
+
+    const _userRoles = {
+      roles: roleIds,
+    };
+    assignUserRoles({
+      id: user.user_id,
+      data: _userRoles,
+      handleOpen,
+      updateUser,
+    });
+
+    const action = mode ? assignUserRoles : removeUserRoles;
+
+    action({
+      id: user.user_id,
+      data: _userRoles,
+      handleOpen,
+      updateUser,
+    });
+  };
+
+  return (
+    <div>
+      <Dialog size="xs" open={open} handler={handleOpen}>
+        <DialogHeader>{mode ? "Asignar roles" : "Remover roles"}</DialogHeader>
+        <DialogBody>
+          <div className="flex flex-col gap-4 items-center">
+            <Avatar src={`${user?.picture}`} className="w-40 h-40"></Avatar>
+            <Input
+              label="Nombre"
+              crossOrigin={""}
+              onChange={(e) => setUserEmail(e.target.value)}
+              defaultValue={user?.name || ""}
+              disabled
+            />
+            <Input
+              label="Correo"
+              crossOrigin={""}
+              onChange={(e) => setUserEmail(e.target.value)}
+              defaultValue={user?.email || ""}
+              disabled
+            />
+            <Select
+              closeMenuOnSelect={false}
+              components={animatedComponents}
+              isMulti
+              placeholder="Seleccione los roles"
+              onChange={(e) => setSelectedRoles(e)}
+              value={selectedRoles}
+              options={mode ? rolesList : selectedRoles}
+              noOptionsMessage={() => "No hay mas roles"}
+              className="w-full"
+            />
+
+            <Button
+              className="bg-navy w-full"
+              disabled={selectedRoles.length > 0 ? false : true}
+              onClick={handleUserSubmit}
+            >
+              {mode ? "Asignar roles" : "Remover roles"}
+            </Button>
+          </div>
+        </DialogBody>
+      </Dialog>
+    </div>
+  );
+}
