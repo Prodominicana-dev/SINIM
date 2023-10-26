ef990e93f1979faf0fe3f1d156e36f0e2b426a02
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-10-26 10:47:09 -0400

    menu mobile

---
+++ src/components/dashboard/mobileDatamarketMenu.tsx
@@ -0,0 +1,74 @@
+import DataMarket from "@/src/models/datamarket";
+import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import {
+  Accordion,
+  ListItem,
+  AccordionHeader,
+  ListItemPrefix,
+  Typography,
+  AccordionBody,
+  List,
+} from "@material-tailwind/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface sidebarMenuProps {
+  title: string;
+  data: DataMarket[];
+}
+
+export default function DataMarketMenu({
+  title,
+  data,
+}: sidebarMenuProps) {
+  const [open, setOpen] = useState(0);
+  const handleOpen = (value: any) => {
+    setOpen(open === value ? 0 : value);
+  };
+  useEffect(() => {
+    setOpen(0);
+  }, [setOpen]);
+  return (
+    <Accordion
+      open={open === 1}
+      icon={
+        <ChevronDownIcon
+          strokeWidth={2.5}
+          className={`mx-auto h-4 w-4 transition-transform text-navy  ${
+            open === 1 ? "rotate-180" : ""
+          }`}
+        />
+      }
+    >
+      <ListItem className="p-0" selected={open === 1}>
+        <AccordionHeader
+          onClick={() => handleOpen(1)}
+          className="p-3 border-b-0 text-navy "
+        >
+          <ListItemPrefix>
+            <div></div>
+          </ListItemPrefix>
+          <Typography
+            className="mr-auto font-normal text-navy"
+          >
+            {title}
+          </Typography>
+        </AccordionHeader>
+      </ListItem>
+      <AccordionBody className="py-1">
+        <List className="p-0 text-navy">
+          {data?.map((datamarket: any, key: number) => (
+            <Link href={`/dashboard/datamarket/${datamarket.id}`} key={key}>
+              <ListItem>
+                <ListItemPrefix>
+                  <ChevronRightIcon strokeWidth={3} className="w-5 h-3" />
+                </ListItemPrefix>
+                {datamarket.title}
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </AccordionBody>
+    </Accordion>
+  );
+}
--- src/components/dashboard/mobileMenu.tsx
+++ src/components/dashboard/mobileMenu.tsx
@@ -6,47 +6,76 @@ import {
   BellAlertIcon,
   ExclamationCircleIcon,
   ArrowLeftOnRectangleIcon,
+  ChevronDownIcon,
+  PresentationChartBarIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/24/outline";
 import {
   Typography,
   IconButton,
   Drawer,
   Avatar,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  List,
+  ListItem,
+  ListItemPrefix,
 } from "@material-tailwind/react";
 import { Fragment, useState } from "react";
 import Suscribe from "../saim/Suscribe/suscribe";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import Image from "next/image";
+import DataMarketIcon from "../svg/datamarket";
+import { useDataMarketsCategories } from "@/src/services/datamarket/service";
+import DataMarketMenu from "./mobileDatamarketMenu";
+import RamiIcon from "../svg/rami";
+import SaimIcon from "../svg/saim";
+import SiedIcon from "../svg/sied";
+import SiedSubscribe from "../sied/Suscribe/suscribe";
+import Login from "../validate/login";
 
 export default function MobileMenu({ isOpen, onClose }: any) {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   const pathname = usePathname();
   const callbackUrl = `${baseUrl}${pathname}`;
   const navigationOptions = [
-    { href: "#", icon: <UserCircleIcon />, text: "Perfíl" },
-    { href: "#", icon: <ChartBarIcon />, text: "DataMarket" },
-    { href: "/dashboard/rami", icon: <RectangleStackIcon />, text: "RAMI" },
-    { href: "/dashboard/saim", icon: <BellAlertIcon />, text: "SAIM" },
-    { href: "/dashboard/sied", icon: <ExclamationCircleIcon />, text: "SIED" },
-    {
-      href: `/api/auth/logout?returnTo=${encodeURIComponent(callbackUrl)}`,
-      icon: <ArrowLeftOnRectangleIcon />,
-      text: "Cerrar sesión",
-    },
+    { href: "/dashboard/rami", icon: <RamiIcon color="navy" />, text: "RAMI" },
+    { href: "/dashboard/saim", icon: <SaimIcon color="navy" />, text: "Alertas Comerciales" },
+    { href: "/dashboard/sied", icon: <SiedIcon color="navy" />, text: "Alertas de IED" },
+    // {
+    //   href: `/api/auth/logout?returnTo=${encodeURIComponent(callbackUrl)}`,
+    //   icon: <ArrowLeftOnRectangleIcon />,
+    //   text: "Cerrar sesión",
+    // },
   ];
-  const { user, error, isLoading } = useUser();
+  const { user, error, isLoading: userLoading } = useUser();
+  const saimCallbackUrl = `${baseUrl}/dashboard/saim`;
+  const siedCallbackUrl = `${baseUrl}/dashboard/sied`;
+  const [suscribeSied, setSuscribeSied] = useState(false);
+  const router = useRouter();
+  const handleSiedSuscribeOpen = () => {
+    if (!user) return ( <Login />)
+    setSuscribeSied(!suscribeSied);
+  };
   const [suscribeOpen, setSuscribeOpen] = useState(false);
+  const { data, isLoading, isError }: any = useDataMarketsCategories();
   const handleSuscribeOpen = () => {
+    if (!user) return ( <Login />)
     setSuscribeOpen(!suscribeOpen);
   };
+
+  const [open, setOpen] = useState(0);
+  const handleOpen = (value : number) => setOpen(open === value ? 0 : value);
   return (
     <Fragment>
       <Drawer
         open={isOpen}
         onClose={onClose}
         placement="right"
-        className="z-[9999] h-screen flex flex-col justify-between"
+        className="z-[9999] flex flex-col overflow-y-auto"
       >
         <div className="flex flex-col items-center justify-between bg-[url('/images/logo/accountLog.jpg')]">
           <div className="flex flex-row items-center justify-between w-full px-4 pt-2">
@@ -75,17 +104,73 @@ export default function MobileMenu({ isOpen, onClose }: any) {
             )}
           </div>
         </div>
-        <div className="flex flex-col gap-4 p-2 h-4/6">
-          {navigationOptions.map((option, index) => (
-            <NavigationLink
-              key={index}
-              option={option}
-              onClose={onClose}
-              isActive={pathname.includes(option.text.toLowerCase())}
+        <div className="flex flex-col h-full gap-4 p-2">
+       <List className="flex flex-col justify-between h-full">
+       <div>
+       <Accordion
+          open={open === 1}
+          icon={
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
             />
-          ))}
-        </div>
-        <div className="p-2">
+          }
+        >
+          <ListItem className="p-0" selected={open === 1}>
+            <AccordionHeader onClick={() => handleOpen(1)} className="p-3 border-b-0">
+              <ListItemPrefix>
+              <DataMarketIcon color="navy"/>
+              </ListItemPrefix>
+              <Typography className="mr-auto font-normal text-navy">
+                Datamarket
+              </Typography>
+            </AccordionHeader>
+          </ListItem>
+          <AccordionBody className="py-1">
+          <List className="p-0 text-navy">
+              {!isLoading ? (
+                data?.map((datamarket: any, key: number) =>
+                  datamarket.data.length > 1 ? (
+                    <DataMarketMenu 
+                      title={datamarket.category}
+                      data={datamarket.data}
+                      key={key}/>
+                  ) : (
+                    <Link
+                      href={`/dashboard/datamarket/${datamarket.data[0].id}`}
+                      key={key}
+                    >
+                      <ListItem>
+                        <ListItemPrefix>
+                          <div></div>
+                        </ListItemPrefix>
+                        {datamarket.data[0].title}
+                      </ListItem>
+                    </Link>
+                  )
+                )
+              ) : (
+                <ListItem>Loading...</ListItem>
+              )}
+            </List>
+          </AccordionBody>
+        </Accordion>
+        {navigationOptions.map((option: any, key: number) =>(
+          <Link href={option.href}>
+                <ListItem className="focus:bg-transparent">
+                  <ListItemPrefix> 
+                    {option.icon}
+                  </ListItemPrefix>
+                  <Typography
+                    className="mr-auto font-normal text-navy"
+                  >
+                    {option.text}
+                  </Typography>
+                </ListItem>
+              </Link>
+        ))}
+       </div>
+        <div className="p-2 space-y-4">
           {pathname === "/dashboard/saim" ? (
             <>
               <button
@@ -93,13 +178,33 @@ export default function MobileMenu({ isOpen, onClose }: any) {
                   handleSuscribeOpen();
                   onClose();
                 }}
-                className="w-full flex justify-center items-center text-white rounded-xl p-4 h-12 bg-gradient-to-tr from-purple-500 from-[15%] via-sky-600 to-sky-400"
+                className="flex items-center justify-center w-full h-12 p-4 bg-white border-2 text-navy rounded-xl border-navy"
               >
                 Suscríbete
               </button>
             </>
           ) : null}
+          {pathname === "/dashboard/sied" ? (
+            <>
+              <button
+                onClick={() => {
+                  handleSiedSuscribeOpen();
+                  onClose();
+                }}
+                className="w-full flex justify-center items-center text-navy rounded-xl p-4 h-12 bg-white border-2 border-navy from-purple-500 from-[15%] via-sky-600 to-sky-400"
+              >
+                Suscríbete
+              </button>
+            </>
+          ) : null}
+          <Link href={user ? `/api/auth/logout?returnTo=${encodeURIComponent(callbackUrl)}` : `/api/auth/login?returnTo=${callbackUrl}`}
+            className="flex items-center justify-center w-full h-12 p-4 text-white rounded-xl bg-navy">
+                {user ? "Cerrar sesión" : "Iniciar sesión"}
+          </Link>
         </div>
+       </List>
+        </div>
+        
         {suscribeOpen && user ? (
           <Suscribe
             open={suscribeOpen}
@@ -107,25 +212,16 @@ export default function MobileMenu({ isOpen, onClose }: any) {
             email={user.email ?? ""}
           />
         ) : null}
+         {suscribeSied && user ? (
+        <SiedSubscribe
+        open={suscribeSied} 
+        handleOpen={handleSiedSuscribeOpen}
+        email={user.email ?? ""}
+        />
+      ) : null}
       </Drawer>
     </Fragment>
   );
 }
 
-function NavigationLink({ option, isActive, onClose }: any) {
-  const linkClasses = `flex flex-row items-center justify-start w-full p-4 gap-3 text-center bg-transparent shadow-none h-12 rounded-lg ${
-    isActive ? "bg-navy/90 text-white" : "text-navy"
-  }`;
 
-  const iconClasses = `w-4 h-4 ${isActive ? "text-white" : "text-navy"}`;
-  const textClasses = isActive
-    ? "text-white font-normal"
-    : "text-black font-thin";
-
-  return (
-    <Link href={option.href} className={linkClasses} onClick={onClose}>
-      {option.icon && <option.icon.type className={iconClasses} />}
-      <p className={textClasses}>{option.text}</p>
-    </Link>
-  );
-}
