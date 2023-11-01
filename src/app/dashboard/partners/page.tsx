bc9a42b08efd4fb1302df9e493577ab0e9634786
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-11-01 13:03:02 -0400

    Fuentes externas implement

---
+++ src/app/dashboard/partners/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+import Header from '@/src/components/home/header'
+import Card from '@/src/components/partner/card';
+import NotFound from '@/src/components/validate/notFound';
+import { usePartners } from '@/src/services/partners/service'
+import { AdjustmentsHorizontalIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Spinner } from '@material-tailwind/react';
+import React, { useEffect, useState } from 'react'
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { nfd } from "unorm";
+const animatedComponents = makeAnimated();
+
+export default function page() {
+    const { data, isLoading, isError, refetch } = usePartners();
+    const [partners, setPartners] = useState<any[]>([]);
+    const [titleSearch, setTitleSearch] = useState("");
+    const [categorySearch, setCategorySearch] = useState<any>({label: "Categoría...", value: ""});
+    const [categories] = useState<any[]>([{label: "Nacional", value: "nacional"}, {label: "Internacional", value: "internacional"}]);
+    const [isVisible, setIsVisible] = useState(false);
+    const [partnersFiltered, setPartnersFiltered] = useState<any[]>([]);
+    useEffect(() => {
+        // Filtros iniciales
+        let filteredRamis = partners;  
+        // Filtrar por país si se ha proporcionado una búsqueda de país
+        if (titleSearch) {
+          const titleSearchLower = titleSearch.toLowerCase();
+          filteredRamis = filteredRamis.filter((source) => {
+            const title = nfd(source.title.toLowerCase());
+            return title.includes(titleSearchLower);
+          });
+        }
+    
+        if(categorySearch.value){
+          filteredRamis = filteredRamis.filter((source) => {
+            const category = nfd(source.type.toLowerCase());
+            return category === categorySearch.value;
+          });
+        }
+        setPartnersFiltered(filteredRamis);
+      }, [partners, titleSearch, categorySearch]);
+    const handleVisibility = () => {
+        setIsVisible(!isVisible);
+      }
+      const filterVisible = isVisible ? `block` : `hidden`
+
+    useEffect(() => {
+        setPartners(data);
+      }, [data]);
+
+      if(isLoading) return (
+        <div className='flex flex-col w-full h-[90vh] items-center justify-center'>
+            <Spinner />
+        </div>
+      )
+      
+  return (
+    <>
+    <Header
+        title="Fuentes Externas"
+        message="Nuestra plataforma se nutre de una red diversa de fuentes externas para proporcionarte información precisa y actualizada. Explora enlaces a recursos de confianza y expertos en una variedad de campos, desde noticias y tendencias hasta investigaciones y datos relevantes."
+      />
+      <div className='flex flex-col w-full p-8 space-y-4'>
+        <div className='flex flex-row justify-end w-full'>
+        <button
+            onClick={handleVisibility}
+            className={`text-black flex flex-row justify-center items-center space-x-2 w-full sm:w-44 h-10 text-center bg-white rounded-full hover:shadow-lg font-semibold duration-300 hover:text-black/80 border-2 border-black`}
+          >
+            <AdjustmentsHorizontalIcon className="w-5 h-5" />
+            <div>Filtrar</div>
+          </button>
+        </div>
+        <div className={`${filterVisible} flex flex-col-reverse  sm:flex-row justify-end w-full gap-4 sm:space-x-5 `}>
+      <input
+            type="text"
+            className="w-full px-2 rounded-lg sm:px-5 h-9 sm:w-72 ring-1 ring-gray-300"
+            placeholder="Buscar por título..."
+            value={titleSearch}
+            onChange={(e) => setTitleSearch(e.target.value)}
+          />
+          <div className="w-full sm:w-72">
+            <Select
+            closeMenuOnSelect={true}
+            components={animatedComponents}
+            isMulti={false}
+            placeholder={categorySearch.label === "" ? "Categoría..." : categorySearch.label}
+            value={categorySearch.label}
+            onChange={(e) => setCategorySearch(e)}
+            options={categories}
+          />
+          </div>
+          <div className="flex items-end justify-end -order-last sm:order-none">
+                <button
+                  className="flex items-center justify-center w-10 h-10 text-white duration-300 bg-red-600 rounded-lg hover:bg-red-700"
+                  onClick={() => {
+                    setTitleSearch("");
+                    setCategorySearch({label: "", value: ""});
+                    setIsVisible(false);
+                  }}
+                >
+                  <XMarkIcon className="w-6 h-6 text-white" />
+                </button>
+              </div>
+          
+      </div>
+      <div className='grid grid-cols-1 place-items-center sm:gap-8 sm:p-8 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4'>
+        {partners?.length === 0 ? (<NotFound />) : (
+            <>
+            {titleSearch === "" && categorySearch.label === "" ? (<>
+                {partners?.map((partner) => (
+                <Card partner={partner} />
+            ))}
+            </>) : (<>
+                {partnersFiltered?.length === 0 ? (<NotFound />) : (<>
+                    {partnersFiltered?.map((partner) => (
+                    <Card partner={partner} />
+                ))}
+                </>)}
+            </>)}
+            </>
+        )}
+        
+      </div>
+      </div>
+     
+    </>
+  )
+}
---
+++ src/components/home/header.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function Header({title, message}: {title: string, message: string}) {
+  return (
+    <>
+         <div className="w-full h-[26rem] bg-gradient-to-r from-sky-600 from-[15%] via-green-400 to-green-500 flex flex-col justify-center items-center space-y-6">
+        <div className="text-3xl font-bold text-center text-white lg:text-5xl">
+          {title}
+        </div>
+        <div className="w-10/12 text-base font-thin text-center text-white sm:w-8/12 lg:w-6/12 sm:text-xl">
+          {message}
+        </div>
+      </div>
+    </>
+  )
+}
