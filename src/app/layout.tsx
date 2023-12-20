5246a644f589e023b09f38b964d96eef87ba8f4c
eliampimentel <eliampimentel@prodominicana.gob.do>
2023-12-20 11:06:49 -0400

    implement google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ReactNode } from "react";
 import React from "react";
+import GoogleAnalytics from "../components/googleAnalytics";
 
 export const metadata = {
   title: "SINIM",
@@ -15,7 +16,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <UserProvider>
       <html lang="es">
-        <body>{children}</body>
+        <body>
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+            <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+          ) : (
+            <></>
+          )}
+          {children}
+        </body>
       </html>
     </UserProvider>
   );
